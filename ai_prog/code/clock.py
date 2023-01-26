ed8b7109042b9ec255c14f51761c3abf767bf43d
increpas-cls2 <60905756+increpas-cls2@users.noreply.github.com>
2023-01-26 21:21:12 +0900

    clock edit

--- ai_prog/code/clock.py
+++ ai_prog/code/clock.py
@@ -14,48 +14,23 @@ t.speed(0)
 t.pensize(5)
 t.hideturtle()
 
-def drawClock(h, min, second):
-    t.pensize(3)
+def drawClock():
     for n in range(12):
+        t.penup()
+        t.setheading(0)
+        t.pensize(3)
         t.pencolor('red')
-        t.penup()
-        t.home()
-        t.setheading(0 + 30 * n)
-        t.forward(180)
+        t.setheading(30 * n)
+        t.fd(180)
+        t.setheading(30 * n + 90)
         t.pendown()
-        t.circle(20)
+        t.circle(10)
         t.penup()
         t.home()
-    
-    t.penup()
-    t.pensize(3)
-    t.pencolor('orange')    
-    t.home()
-    t.setheading(90)
-    t.rt(second * 6)
-    t.pendown()
-    t.fd(150)
-
-    t.penup()
-    t.pensize(7)
-    t.pencolor('purple')    
-    t.home()
-    t.setheading(90)
-    t.rt(min * 6)
-    t.pendown()
-    t.fd(110)
-
-    t.penup()
-    t.pensize(12)
-    t.pencolor('white')
-    t.home()
-    t.setheading(90)
-    t.rt(h * 30)
-    t.pendown()
-    t.fd(80)
-
+  
 def draw_second(second):
     t.penup()
+    t.home()
     t.pensize(3)
     t.pencolor('orange')    
     t.home()
@@ -63,10 +38,13 @@ def draw_second(second):
     t.rt(second * 6)
     t.pendown()
     t.fd(150)
+    t.penup()
+    t.home()
     
 
 def draw_min(min):
     t.penup()
+    t.home()
     t.pensize(7)
     t.pencolor('purple')    
     t.home()
@@ -74,9 +52,12 @@ def draw_min(min):
     t.rt(min * 6)
     t.pendown()
     t.fd(110)
+    t.penup()
+    t.home()
 
 def draw_hour(h):
     t.penup()
+    t.home()
     t.pensize(12)
     t.pencolor('white')
     t.home()
@@ -84,13 +65,18 @@ def draw_hour(h):
     t.rt(h * 30)
     t.pendown()
     t.fd(80)
+    t.penup()
+    t.home()
 
 
 while True:
     hour = int(time.strftime('%I'))
     min = int(time.strftime('%M'))
     sec = int(time.strftime('%S'))
-    drawClock(hour,min, sec)
+    drawClock()
+    draw_hour(hour)
+    draw_min(min)
+    draw_second(sec)
     ws.update()
     time.sleep(1)
     t.clear()
